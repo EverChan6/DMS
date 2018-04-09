e3154e6e6ca8d24495f80929d086931fa5bfbb56
EverChan <everchan611@gmail.com>
2018-04-09 20:21:30 +0800

    添加全部历史通知标签页，换掉退换宿申请标签页

--- public/js/studentRole.js
+++ public/js/studentRole.js
@@ -15,7 +15,7 @@
 
 	initTable();
 
-	// 暂且放过你，哼！
+	// 我的业务进度表格
 	function initTable(){
 
 			$("#myProgress").bootstrapTable({
@@ -118,49 +118,30 @@
 
 
 
-	//从数据库取出通知,此处渲染标题及时间
+	//从数据库取出全部通知,此处只渲染五条标题及时间
 	$.ajax(
 	{
-		method: "get",
-		url: "api/getNotice",
+		type: "get",
+		url: "api/getAllNotices",
 		dataType: "json",
 		success: function(result)
 		{
-			//这里处理成功后渲染
-			// console.log(result);
 			if(result.code == 4)
 			{
 				// console.log(result.message);					//服务端返回的信息
-				//注意这是个全局变量
+
+				//注意这是个全局变量，因为后面点击具体通知的时候会用到
 				data = result.data;								//要渲染的数据（包括标题、发布单位、时间和内容
-				let $anotice = $(".notice");					//取得a标签，以渲染标题
-				// console.log($anotice);
-				let $span = $(".time");							//取得span标签，以渲染时间
-				// console.log($span);
 
-				//轮播图的h3和p
-				let $h3 = $(".carousel-caption h3");
-				// console.log($h3);
-				let $p = $(".carousel-caption p");
-				// console.log($p);
+				//这里渲染首页最新通知
+				showHome(data);
 
-				//将数组反序（因为要先显示最新的
-				data.reverse();
+				//这里渲染全部历史通知公告
+				showHistory(data);
 
-				for(let i = 0; i < data.length; i ++)
-				{
-					// console.log($anotice[i].innerText);		//jQuery .text()方法前面一定是个dom对象才能用。
-					$anotice[i].innerText = data[i].title;		//通知的标题
-					$span[i].innerText = data[i].time;			//通知的发布时间
 
-					//轮播图的h3和p
-					if(i < 3)			//因为轮播图值显示3张
-					{
-						$h3[i].innerText = data[i].title;
-						$p[i].innerText = data[i].time;
-					}
-					
-				}//end-for
+
+
 			}//end-if
 
 		},
@@ -168,10 +149,57 @@
 		{
 			console.log(e.status);
 		}
-	});
+	});//end-ajax
 
 
 
+	function showHome(data)
+	{
+		let $anotice = $(".notice");					//取得a标签，以渲染标题
+		// console.log($anotice);
+		let $span = $(".time");							//取得span标签，以渲染时间
+		// console.log($span);
+
+		//轮播图的h3和p
+		let $h3 = $(".carousel-caption h3");
+		// console.log($h3);
+		let $p = $(".carousel-caption p");
+		// console.log($p);
+
+		//不用了，后台已经按照降序查找最近的记录了
+		//将数组反序（因为要先显示最新的
+		// data.reverse();
+
+		for(let i = 0; i < 5; i ++)						//首页只显示5条
+		{
+			// console.log($anotice[i].innerText);		//jQuery .text()方法前面一定是个dom对象才能用。
+			$anotice[i].innerText = data[i].title;		//通知的标题
+			$span[i].innerText = data[i].time;			//通知的发布时间
+
+			//轮播图的h3和p
+			if(i < 3)			//因为轮播图只显示3张
+			{
+				$h3[i].innerText = data[i].title;
+				$p[i].innerText = data[i].time;
+			}
+			
+		}//end-for
+
+	}
+
+	
+	function showHistory(data)
+	{
+		var histories = "";
+		for(let i = 0; i < data.length; i ++)
+		{
+			//拼接
+			histories += '<li><a href="#" class="notice">'+data[i].title+'</a><span class="time">'+data[i].time+'</span></li>'
+		}
+		console.log(histories);
+		//插入DOM
+		$("#allNotices .panel-body ul").append(histories);
+	}
 
 
 
@@ -190,7 +218,7 @@
 		//取得点击通知的标题
 		// var title = target.text();
 		var title = target.innerText;
-		console.log("title: "+title);
+		// console.log("title: "+title);
 
 		//先把原来的保存下来，以便恢复
 		var $home = $("#home");
@@ -203,7 +231,7 @@
 		//取得具体通知的内容，放进html标签里
 		for(let i = 0; i < data.length; i ++)
 		{
-			if(title == data[i].title)
+			if(title == data[i].title)	//匹配
 			{
 				$("#showTitle").text(data[i].title);
 				$("#showContents").text(data[i].contents);
@@ -223,22 +251,31 @@
 		
 
 		//点击返回，则回到上一页
-		$("#return").click(function()
+		$("#return").click(back);
+		function back()
 		{
+			console.log("这是back里的return函数");
 			//听说如果是表单，go会保留表单数据，而back不会（好像会刷新页面
 			// history.go(-1);
 
 			//要的效果应该是回到上一个面板，就首页通知，上面那个不行！
 			$ph.html($preHeading);
 			$pb.html($preBody);
-			//重新注册点击事件。。。
-			$(".notice").on("click", showNotice);
 
-		});
+
+			//重新注册点击事件
+			//这是从首页点击具体通知后返回首页
+			$(".notice").on("click", showNotice);
+			$("#more").on("click", showAllNotices);
+			
+			//这是从（更多里的）全部通知点击具体通知后返回全部通知
+			$("#return").on("click", back);
+		}
 
 
 	};//end-showNotice
 	
+
 
 	
 
@@ -386,7 +423,7 @@
 
  		//打包数据
  		var fixFormData = processData("fixForm");
- 		console.log(fixFormData);
+ 		// console.log(fixFormData);
 
  		//发送给后台
  		$.ajax(
@@ -524,9 +561,9 @@
 
  		},
  		'click #done': function(e, value, row, index){
- 			console.log("您点击了完成按钮");
 
- 			//把该项记录在数据库记录的status子段设置为"已处理"，本来应该是"未处理"
+
+ 			//把该项记录在数据库记录的status字段设置为"已处理"，本来应该是"未处理"
  			$.ajax(
  			{
  				method: "post",
@@ -536,10 +573,12 @@
  				success: function(result)
  				{
  					console.log(result.message);
+ 					//把删除跟完成按钮变成已处理
+ 					e.target.parentNode.innerHTML = "<span>已处理</span>";
  				},
- 				error: function(result)
+ 				error: function(error)
  				{
- 					console.log(result.message)
+ 					console.log(error);
  				}
 
  			});
